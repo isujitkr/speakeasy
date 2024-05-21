423d6aaaefeca3ba70154991a86e429ea15ecafb
Sujit Kumar <skr87981@gmail.com>
2024-05-22 00:04:46 +0530

    signup api and validation created

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import { config } from "dotenv";
+import morgan from 'morgan'
+import appRouter from "./routes/index.js";
 
 config();
 const app = express();
@@ -8,5 +10,10 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(cors());
+
+//remove it in production
+app.use(morgan('dev'));
+
+app.use('/api/v1', appRouter);
 
 export default app;
