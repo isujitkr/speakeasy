1bcd36182a7917483775aab4c3ec8cbaf80d3dde
Sujit Kumar <skr87981@gmail.com>
2024-05-22 01:59:41 +0530

    implemented jwt and httpOnly cookie

--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
 import { hash, compare } from 'bcrypt';
+import { createToken } from "../utils/token-manager.js";
+import { COOKIE_NAME } from "../utils/constants.js";
 
 export const getAllUsers = async (req:Request,res:Response,next: NextFunction) => {
     //get all users
@@ -30,6 +32,27 @@ export const userSignup = async (
       const user = new User({ name, email, password: hashedPassword });
       await user.save();
 
+      //create token and store cookie
+      res.clearCookie(COOKIE_NAME, {
+        httpOnly: true,
+        domain: "localhost",
+        signed: true,
+        path: "/",
+      });
+
+      const token = createToken(user._id.toString(), user.email, "7d");
+
+      const expires = new Date();
+      expires.setDate(expires.getDate() + 7);
+
+      res.cookie(COOKIE_NAME, token, {
+        path: "/",
+        domain: "localhost",
+        expires,
+        httpOnly: true,
+        signed: true,
+      });
+
       return res.status(201).json({ message: "OK", id:user._id.toString() });
   } catch (error) {
     console.log(error);
@@ -55,6 +78,20 @@ export const userLogin = async (
     if (!isPasswordCorrect) {
       return res.status(403).send('Incorrect Credentials');
     }
+
+    res.clearCookie(COOKIE_NAME, {
+      httpOnly: true,
+      domain: 'localhost',
+      signed: true,
+      path: '/',
+    });
+
+    const token = createToken(user._id.toString(), user.email, '7d');
+
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+
+    res.cookie(COOKIE_NAME, token,{path:'/',domain:'localhost',expires,httpOnly: true,signed:true});
 
     return res.status(200).json({ message: "OK", id: user._id.toString() });
 
